35b6c1d52bc95d33d25d9edef1d4090b43ae4a4e
kogutserg <kogutserg@gmail.com>
2020-11-27 18:44:39 +0200

    Code 7 (#5)
    Co-authored-by: Sergii Kogut <skogut@playtika.com>

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-if="!profile">Need to authorize via <a href="/login">Google</a></div>
+    <div v-else>
+      <div>{{ profile.name }}&nbsp;<a href="/logout">Log Out</a></div>
+      <div>&nbsp;</div>
+      <messages-list :messages="messages"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MessagesList from "components/messages/MessageList.vue";
+
+  export default {
+    components: {
+      MessagesList
+    },
+    data() {
+      return {
+        messages: frontendData.messages,
+        profile: frontendData.profile
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
