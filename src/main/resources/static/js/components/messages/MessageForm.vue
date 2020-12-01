026d248a32a013d1cff53af9d2b5f0607f4f77c4
kogutserg <kogutserg@gmail.com>
2020-12-01 23:42:00 +0200

    Code 9 (#7)
    Co-authored-by: Sergii Kogut <skogut@playtika.com>

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <input type="text" placeholder="Write something" v-model="text"/>
-    <input type="button" value="Save" @click="save"/>
-  </div>
+  <v-layout row>
+    <v-text-field
+        label="New message"
+        placeholder="Write something"
+        v-model="text"
+    />
+    <v-btn @click="save">
+      Save
+    </v-btn>
+  </v-layout>
 </template>
 
 <script>
 import {sendMessage} from 'util/ws';
-
 
 
 export default {
@@ -29,25 +34,25 @@ export default {
       sendMessage({id: this.id, text: this.text})
       this.text = ''
       this.id = ''
-  /*save() {
-    const message = {text: this.text}
+      /*save() {
+        const message = {text: this.text}
 
-    if (this.id) {
-      this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
-          result.json().then(data => {
-            const index = getIndex(this.messages, data.id)
-            this.messages.splice(index, 1, data)
-            this.text = ''
-            this.id = ''
-          })
-      )
-    } else {
-      this.$resource('/message{/id}').save({}, message).then(result =>
-          result.json().then(data => {
-            this.messages.push(data)
-            this.text = ''
-          })
-      )*/
+        if (this.id) {
+          this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
+              result.json().then(data => {
+                const index = getIndex(this.messages, data.id)
+                this.messages.splice(index, 1, data)
+                this.text = ''
+                this.id = ''
+              })
+          )
+        } else {
+          this.$resource('/message{/id}').save({}, message).then(result =>
+              result.json().then(data => {
+                this.messages.push(data)
+                this.text = ''
+              })
+          )*/
     }
   }
 }
