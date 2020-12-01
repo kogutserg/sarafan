5e886eda2e6569ef530eebff32841c5de56c95c4
Sergii Kogut <skogut@playtika.com>
2020-12-01 23:40:30 +0200

    Code 9

--- src/main/java/com/javalearn/sarafan/controller/MainController.java
+++ src/main/java/com/javalearn/sarafan/controller/MainController.java
@@ -28,8 +28,15 @@ public class MainController {
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
         HashMap<Object, Object> data = new HashMap<>();
+
+        if (user != null) {
+            data.put("profile", user);
+            data.put("messages", messageRepo.findAll());
+        }
+
         data.put("profile", user);
         data.put("messages", messageRepo.findAll());
+
         model.addAttribute("frontendData", data);
         model.addAttribute("isDevMode", "dev".equals(profile));
         return "index";
